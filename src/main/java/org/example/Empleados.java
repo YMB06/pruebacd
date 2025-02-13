76ca3225324cf8a7764668a10d55667835e3801a
YMB06 <152322438+YMB06@users.noreply.github.com>
2025-02-13 08:50:18 +0100

    Update Empleados.java quitar comentarios innecesarios

--- src/main/java/org/example/Empleados.java
+++ src/main/java/org/example/Empleados.java
@@ -47,12 +47,3 @@ public class Empleados {
 }
 
 
-/*Método | Especificación
-
-float calculaSalarioBruto(TipoEmpleado tipo, float ventasMes, float horasExtra)
-El salario base será 1000 euros si el empleado es de tipo TipoEmpleado.vendedor, y de 1500 euros si es de tipo TipoVendedor.encargado. A esta cantidad se le sumará una prima de 100 euros si ventasMes es mayor o igual que 1000 euros, y de 200 euros si fuese al menos de 1500 euros. Por último, cada hora extra se pagará a 20 euros. Si tipo es null, o ventasMes o horasExtra toman valores negativos, el método lanzará una excepción de tipo BRException.
-
-float calculaSalarioNeto(float salarioBruto)
-Si el salario bruto es menor de 1000 euros, no se aplicará ninguna retención.
-Para salarios a partir de 1000 euros, y menores de 1500 euros, se les aplicará un 16%, y a los salarios a partir de 1500 euros se les aplicará un 18%.
-El método nos devolverá salarioBruto * (1 - retencion), o BRException si el salario es menor que cero.*/
